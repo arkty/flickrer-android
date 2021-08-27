f35a606a293a9f0e204e3ac7d96a9a181874218f
Andrey Khitryy <andrey.khitryy@gmail.com>
2021-08-27 21:23:52 +0300

    TF image processing fix

--- app/src/main/java/me/arkty/flickrer/MainActivity.kt
+++ app/src/main/java/me/arkty/flickrer/MainActivity.kt
@@ -6,12 +6,17 @@ import dagger.hilt.android.AndroidEntryPoint
 import me.arkty.flickrer.core.android.BaseActivity
 import me.arkty.flickrer.core.android.utils.LayoutInflation
 import me.arkty.flickrer.databinding.ActivityMainBinding
+import me.arkty.flickrer.utils.helper.NsfwDetectHelper
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override val layout: LayoutInflation<ActivityMainBinding> = ActivityMainBinding::inflate
     override val vm by viewModels<MainViewModel>()
+
+    @Inject
+    lateinit var detector: NsfwDetectHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Switch from Splash to Normal theme
--- app/src/main/java/me/arkty/flickrer/utils/helper/NsfwDetectHelper.kt
+++ app/src/main/java/me/arkty/flickrer/utils/helper/NsfwDetectHelper.kt
@@ -2,15 +2,19 @@ package me.arkty.flickrer.utils.helper
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.media.ThumbnailUtils
 import dagger.hilt.android.qualifiers.ApplicationContext
 import me.arkty.flickrer.ml.SavedModel
 import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.common.ops.QuantizeOp
 import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.image.ops.ResizeOp
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.math.min
 
 @Singleton
 class NsfwDetectHelper @Inject constructor(
@@ -20,18 +24,28 @@ class NsfwDetectHelper @Inject constructor(
 
     // type: float32[1,224,224,3]
     private val imageProcessor = ImageProcessor.Builder()
-        .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
+        .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+        .add(NormalizeOp(0f, 255f))
         .build()
 
-    fun test(b: Bitmap) {
+    fun isSafeForWork(b: Bitmap, tag: String = "") : Boolean {
+        synchronized(NsfwDetectHelper::class) {
+            val size = min(b.width, b.height)
+            val squared = ThumbnailUtils.extractThumbnail(b, size, size)
 
-        val buffer = TensorImage(DataType.FLOAT32).let {
-            it.load(b)
-            imageProcessor.process(it)
-        }.tensorBuffer
+            val buffer = TensorImage(DataType.FLOAT32).let {
+                it.load(squared)
+                imageProcessor.process(it)
+            }.tensorBuffer
+            //Timber.d("buffer = ${buffer.floatArray.joinToString(",")}")
+            val outputs = model.process(buffer)
+            val probability = outputs.outputFeature0AsTensorBuffer
+            Timber.d("probability[$tag]: ${probability.floatArray.joinToString(", ")}")
+        }
+        return true
+    }
 
-        val outputs = model.process(buffer)
-        val probability = outputs.outputFeature0AsTensorBuffer
-        Timber.d("probability: ${probability.floatArray.joinToString(", ")}")
+    companion object{
+        //const val LABELS = listOf()
     }
 }
